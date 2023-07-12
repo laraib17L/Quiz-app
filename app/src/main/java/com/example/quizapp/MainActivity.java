bd6cc672a5aa4872355f70f93cffb7e66814b02a
laraib17L <125170688+laraib17L@users.noreply.github.com>
2023-07-12 09:07:29 -0700

    commit 3

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -1,14 +1,98 @@
 package com.example.quizapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolbar);
+
+        //setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,R.string.OpenDrawer,R.string.CloseDrawer);
+
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id= item.getItemId();
+
+            if(id==R.id.test)
+            {
+                loadFragment1(new AFragment());
+            }else  {
+                loadFragment(new QuizResultFragment());
+            }
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
+    }
+
+
+    @Override
+
+    public void onBackPressed()
+    {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    private void loadFragment(QuizResultFragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        // Remove the existing fragment, if any
+        Fragment existingFragment = fm.findFragmentById(R.id.container);
+        if (existingFragment != null) {
+            ft.remove(existingFragment);
+        }
+
+        ft.replace(R.id.container, fragment);
+        ft.commit();
+    }
+    private void loadFragment1(AFragment fragment)
+    {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        // Remove the existing fragment, if any
+        Fragment existingFragment = fm.findFragmentById(R.id.container);
+        if (existingFragment != null) {
+            ft.remove(existingFragment);
+        }
+
+        ft.add(R.id.container, fragment);
+        ft.commit();
     }
 }
